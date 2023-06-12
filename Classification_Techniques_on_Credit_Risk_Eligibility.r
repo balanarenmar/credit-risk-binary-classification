ab79d604128ccceec7412dd443de94944af543f7
RenmarB <renmarfrancia.balana@bicol-u.edu.ph>
2023-06-13 01:18:39 +0800

    Improved Code Readability

--- Classification_Techniques_on_Credit_Risk_Eligibility.r
+++ Classification_Techniques_on_Credit_Risk_Eligibility.r
@@ -1,3 +1,5 @@
+# Import Packages
+
 library(tidyverse)
 library(DescTools)
 library(rpart.plot)
@@ -16,27 +18,32 @@ library(corrplot)
 set.seed(0)
 options("scipen"=100, "digits"=5)
 
-new_card_data <- read.csv("C:\\Users\\Olan\\Documents\\Olan\\3rd_year\\2nd_semester\\Data_Mining\\Case_study_Algos\\new_card_data.csv")
-new_card_data$loan_status <- factor(new_card_data$loan_status)
-new_card_data$initial_list_status <- factor(new_card_data$initial_list_status)
+#Load the csv file "data.unbalanced"
+data.unbalanced <- read.csv("new_card_data.csv")
+data.unbalanced$loan_status <- factor(data.unbalanced$loan_status)
+data.unbalanced$initial_list_status <- factor(data.unbalanced$initial_list_status)
 
-library(RSBID)
-#data.smotenc <- SMOTE_NC(new_card_data, 'loan_status')
-data.smotenc <- read.csv("datasmotenc.csv")
-data.smotenc$loan_status <- factor(data.smotenc$loan_status)
-data.smotenc$initial_list_status <- factor(data.smotenc$initial_list_status)
+#library(RSBID)
+#data.balanced <- SMOTE_NC(data.unbalanced, 'loan_status')
+
+#datasmotenc contains the balanced data set after performing SMOTE-NC method.
+data.balanced <- read.csv("datasmotenc.csv")
+
+#change to proper data type to perform data mining
+data.balanced$loan_status <- factor(data.balanced$loan_status)
+data.balanced$initial_list_status <- factor(data.balanced$initial_list_status)
 
 
 ########## SPLITTING ###########
-inTrain <- createDataPartition(y = new_card_data$loan_status, p = .7, list = FALSE)
-new_card_data_train <- new_card_data %>% slice(inTrain)
-new_card_data_test <- new_card_data %>% slice(-inTrain)
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .5, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrain <- createDataPartition(y = data.unbalanced$loan_status, p = .7, list = FALSE)
+data.unbalanced_train <- data.unbalanced %>% slice(inTrain)
+data.unbalanced_test <- data.unbalanced %>% slice(-inTrain)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .5, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 #k is number of fold in cross validation
-train_index <- createFolds(new_card_data_train$loan_status, k = 10)
+train_index <- createFolds(data.unbalanced_train$loan_status, k = 10)
 
 #Enable parallel processing to speed up training
 library(doParallel)
@@ -44,20 +51,20 @@ cl <- makePSOCKcluster(5)
 registerDoParallel(cl)
 
 
-##  !!!   Replace data.smotenc with new_card_data.
+##  !!!   Replace data.balanced with data.unbalanced.
 ##  !!!    for balanced and unbalanced respectively
 
 ##### RPART Decision Tree ###########
 
 #k determines number of folds
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 50)
+train_index <- createFolds(data.balanced$loan_status, k = 50)
 
 #fine tuning parameter. seq(start, end, interval) or explicit
 #tuneGrid = data.frame(cp = seq(0, 0.08, 0.001)), 
 rpartGrid = data.frame(cp = 0)
 
-rpartFit <- data.smotenc %>%
+rpartFit <- data.balanced %>%
   train(loan_status ~ .,
         data = . ,
         method = "rpart",
@@ -69,14 +76,14 @@ rpartFit <- data.smotenc %>%
 #rpartFit
 confusionMatrix(rpartFit$pred$pred, rpartFit$pred$obs, mode= "everything")
 
-summary(data.smotenc)
+summary(data.balanced)
 
 #------------- Template for hold-out method ------------#
 #NOTE: for different splits, edit the splitting code above
 
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .6, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .6, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 rpartFitH <- train.smotenc %>%
   train(loan_status ~ .,
@@ -93,10 +100,10 @@ cm$overall[1:2]
 
 ########## KNN ##########
 #k determines number of folds
-train_index <- createFolds(data.smotenc$loan_status, k = 10)
+train_index <- createFolds(data.balanced$loan_status, k = 10)
 
 set.seed(0)
-knnFit <- data.smotenc %>% train(loan_status ~ .,
+knnFit <- data.balanced %>% train(loan_status ~ .,
                               method = "knn",
                               data = .,
                               tuneLength = 5,
@@ -108,9 +115,9 @@ knnFit <- data.smotenc %>% train(loan_status ~ .,
 #knnFit
 confusionMatrix(knnFit$pred$pred, knnFit$pred$obs, mode= "everything")
 
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .9, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .9, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 knnFitH <- train.smotenc %>%
   train(loan_status ~ .,
@@ -127,10 +134,10 @@ cm$overall[1:2]
 
 ########## PART - Rule-Based Classifier ###########
 #k determines number of folds
-train_index <- createFolds(data.smotenc$loan_status, k = 20)
+train_index <- createFolds(data.balanced$loan_status, k = 20)
 set.seed(0)
 
-rulesFit <- data.smotenc %>% train(loan_status ~ .,
+rulesFit <- data.balanced %>% train(loan_status ~ .,
                                 method = "PART",
                                 data = .,
                                 tuneLength = 5,
@@ -142,9 +149,9 @@ rulesFit$finalModel #print decision list
 confusionMatrix(rulesFit$pred$pred, rulesFit$pred$obs, mode= "everything")
 
 
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .9, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .9, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 rulesFitH <- train.smotenc %>%
   train(loan_status ~ .,
@@ -161,9 +168,9 @@ rulesFitH$finalModel
 
 ############ Linear Support-Vector-Machine SVM #########
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 20)
+train_index <- createFolds(data.balanced$loan_status, k = 20)
 
-svmFit <- data.smotenc %>% train(loan_status ~.,
+svmFit <- data.balanced %>% train(loan_status ~.,
                               method = "svmLinear",
                               data = .,
                               tuneLength = 5,
@@ -175,30 +182,30 @@ svmFit
 confusionMatrix(svmFit$pred$pred, svmFit$pred$obs, mode= "everything")
 
 
-inTrain <- createDataPartition(y = new_card_data$loan_status, p = .9, list = FALSE)
-new_card_data_train <- new_card_data %>% slice(inTrain)
-new_card_data_test <- new_card_data %>% slice(-inTrain)
+inTrain <- createDataPartition(y = data.unbalanced$loan_status, p = .9, list = FALSE)
+data.unbalanced_train <- data.unbalanced %>% slice(inTrain)
+data.unbalanced_test <- data.unbalanced %>% slice(-inTrain)
 
-svmFitH <- new_card_data_train %>%
+svmFitH <- data.unbalanced_train %>%
   train(loan_status ~ .,
         data = . ,
         method = "svmLinear",
         tuneLength = 5,
         trControl = trainControl(savePredictions = "final"))
-pred <- predict(svmFitH, newdata = new_card_data_test)
-cm<- confusionMatrix(pred, new_card_data_test$loan_status, mode= "everything")
+pred <- predict(svmFitH, newdata = data.unbalanced_test)
+cm<- confusionMatrix(pred, data.unbalanced_test$loan_status, mode= "everything")
 cm$byClass[5:7]
 cm$overall[1:2]
 
 ########### Random Forest #############
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 50)
+train_index <- createFolds(data.balanced$loan_status, k = 50)
 
 #create tunegrid with 8 values from 1:15, for mtry to tuning model. Upper value depend on Tune length
 #Our train function will change number of entry variable at each split according to tunegrid. 
 RFgrid <- expand.grid(.mtry = (1:5))
 
-randomForestFit <- data.smotenc %>% train(loan_status ~ .,
+randomForestFit <- data.balanced %>% train(loan_status ~ .,
                                                  method = "rf",
                                                  data = .,
                                                  tuneLength = 5,
@@ -213,28 +220,28 @@ plot(randomForestFit)
 confusionMatrix(randomForestFit$pred$pred, randomForestFit$pred$obs, mode= "everything")
 
 
-inTrain <- createDataPartition(y = new_card_data$loan_status, p = .9, list = FALSE)
-new_card_data_train <- new_card_data %>% slice(inTrain)
-new_card_data_test <- new_card_data %>% slice(-inTrain)
+inTrain <- createDataPartition(y = data.unbalanced$loan_status, p = .9, list = FALSE)
+data.unbalanced_train <- data.unbalanced %>% slice(inTrain)
+data.unbalanced_test <- data.unbalanced %>% slice(-inTrain)
 
-randomForestFitH <- new_card_data_train %>%
+randomForestFitH <- data.unbalanced_train %>%
   train(loan_status ~ .,
         data = . ,
         method = "rf",
         tuneLength = 5,
         tuneGrid = RFgrid,
         trControl = trainControl(savePredictions = "final"))
-pred <- predict(randomForestFitH, newdata = new_card_data_test)
-cm<- confusionMatrix(pred, new_card_data_test$loan_status, mode= "everything")
+pred <- predict(randomForestFitH, newdata = data.unbalanced_test)
+cm<- confusionMatrix(pred, data.unbalanced_test$loan_status, mode= "everything")
 cm$byClass[5:7]
 cm$overall[1:2]
 randomForestFitH
 ########### Naive Bayes #############
 
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 50)
+train_index <- createFolds(data.balanced$loan_status, k = 50)
 
-nBayesFit <- data.smotenc %>% train(loan_status ~ .,
+nBayesFit <- data.balanced %>% train(loan_status ~ .,
                                      method = "nb",
                                      data = .,
                                      tuneLength = 5,
@@ -248,18 +255,18 @@ nBayesFit <- data.smotenc %>% train(loan_status ~ .,
 confusionMatrix(nBayesFit$pred$pred, nBayesFit$pred$obs, mode= "everything")
 
 
-inTrain <- createDataPartition(y = new_card_data$loan_status, p = .7, list = FALSE)
-new_card_data_train <- new_card_data %>% slice(inTrain)
-new_card_data_test <- new_card_data %>% slice(-inTrain)
+inTrain <- createDataPartition(y = data.unbalanced$loan_status, p = .7, list = FALSE)
+data.unbalanced_train <- data.unbalanced %>% slice(inTrain)
+data.unbalanced_test <- data.unbalanced %>% slice(-inTrain)
 
-nBayesFitH <- new_card_data_train %>%
+nBayesFitH <- data.unbalanced_train %>%
   train(loan_status ~ .,
         data = . ,
         method = "nb",
         tuneLength = 5,
         trControl = trainControl(savePredictions = "final"))
-pred <- predict(nBayesFitH, newdata = new_card_data_test)
-cm<- confusionMatrix(pred, new_card_data_test$loan_status, mode= "everything")
+pred <- predict(nBayesFitH, newdata = data.unbalanced_test)
+cm<- confusionMatrix(pred, data.unbalanced_test$loan_status, mode= "everything")
 cm$byClass[5:7]
 cm$overall[1:2]
 
@@ -267,12 +274,12 @@ nBayesFitH
 
 ############ Artificial Neural Network #############
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 10)
+train_index <- createFolds(data.balanced$loan_status, k = 10)
 
 nnetGrid <-  expand.grid(size = seq(from = 1, to = 18, by = 3),
                          decay = seq(from = 0.1, to = 0.5, by = 0.1))
 
-nnetFit <- data.smotenc %>% train(loan_status ~ .,
+nnetFit <- data.balanced %>% train(loan_status ~ .,
                                   method = "nnet",
                                   data = .,
                                   tuneGrid = nnetGrid,
@@ -286,29 +293,29 @@ nnetFit$finalModel
 
 confusionMatrix(nnetFit$pred$pred, nnetFit$pred$obs, mode= "everything")
 
-inTrain <- createDataPartition(y = new_card_data$loan_status, p = .8, list = FALSE)
-new_card_data_train <- new_card_data %>% slice(inTrain)
-new_card_data_test <- new_card_data %>% slice(-inTrain)
+inTrain <- createDataPartition(y = data.unbalanced$loan_status, p = .8, list = FALSE)
+data.unbalanced_train <- data.unbalanced %>% slice(inTrain)
+data.unbalanced_test <- data.unbalanced %>% slice(-inTrain)
 
 nnetGrid <-  expand.grid(size = seq(from = 1, to = 18, by = 3),
                          decay = seq(from = 0.1, to = 0.5, by = 0.1))
 
-nnetFitH <- new_card_data_train %>%
+nnetFitH <- data.unbalanced_train %>%
   train(loan_status ~ .,
         data = . ,
         method = "nnet",
         tuneGrid = nnetGrid,
         trControl = trainControl(savePredictions = "final"))
-pred <- predict(nnetFitH, newdata = new_card_data_test)
-confusionMatrix(pred, new_card_data_test$loan_status, mode= "everything")
+pred <- predict(nnetFitH, newdata = data.unbalanced_test)
+confusionMatrix(pred, data.unbalanced_test$loan_status, mode= "everything")
 
 plot(nnetFitH)
 
 #---------------------------    EXXXTRA    --------------------------#
 ####### Conditional Inference Tree ########
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 20)
-ctreeFit <- data.smotenc %>% train(loan_status ~ .,
+train_index <- createFolds(data.balanced$loan_status, k = 20)
+ctreeFit <- data.balanced %>% train(loan_status ~ .,
                                           method = "ctree",
                                           data = .,
                                           tuneLength = 5,
@@ -321,9 +328,9 @@ cm$byClass[5:7]
 cm$overall[1:2]
 
 
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .6, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .6, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 ctreeFitH <- train.smotenc %>% train(loan_status ~ .,
                                     method = "ctree",
@@ -341,8 +348,8 @@ cm
 ############## C45 Fit ################
 library(RWeka)
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 50)
-C45Fit <- data.smotenc %>% train(loan_status ~ .,
+train_index <- createFolds(data.balanced$loan_status, k = 50)
+C45Fit <- data.balanced %>% train(loan_status ~ .,
                               method = "J48",
                               data = .,
                               tuneLength = 5,
@@ -357,9 +364,9 @@ cm$overall[1:2]
 
 
 
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .9, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .9, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 C45FitH <- train.smotenc %>% train(loan_status ~ .,
                                         method = "J48",
@@ -375,8 +382,8 @@ cm$overall[1:2]
 ########## Gradient Boosted Decision Tree ###########
 library(xgboost)
 set.seed(0)
-train_index <- createFolds(data.smotenc$loan_status, k = 50)
-xgboostFit <- data.smotenc %>% train(loan_status ~ .,
+train_index <- createFolds(data.balanced$loan_status, k = 50)
+xgboostFit <- data.balanced %>% train(loan_status ~ .,
                                   method = "xgbTree",
                                   data = .,
                                   tuneLength = 5,
@@ -400,9 +407,9 @@ cm$overall[1:2]
 
 
 set.seed(0)
-inTrainB <- createDataPartition(y = data.smotenc$loan_status, p = .6, list = FALSE)
-train.smotenc <- data.smotenc %>% slice(inTrainB)
-test.smotenc <- data.smotenc %>% slice(-inTrainB)
+inTrainB <- createDataPartition(y = data.balanced$loan_status, p = .6, list = FALSE)
+train.smotenc <- data.balanced %>% slice(inTrainB)
+test.smotenc <- data.balanced %>% slice(-inTrainB)
 
 xgboostFitH <- train.smotenc %>% train(loan_status ~ .,
                                             method = "xgbTree",
